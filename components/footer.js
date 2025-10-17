b2c3644e07b20a8f100bedc62e123d4886719d30
LucasDSJ <lucasdsjr2005@gmail.com>
2025-10-17 20:14:39 -0300

    Incrementado conteúdo em contato.html

--- components/footer.js
+++ components/footer.js
@@ -3,10 +3,10 @@ const FooterComponent = {
         <footer class="border-top text-center text-white py-3 mt-5" style="background: rgb(71, 53, 142);">
             <div class="border-bottom p-3">
                 <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button">
-                    <i class="fab fa-linkedin-in"></i>
+                    <i class="fab fa-linkedin-in fa-2x"></i>
                 </a>
                 <a class="btn btn-outline-light btn-floating m-1" href="https://github.com/LucasDSJ" target="_blank" role="button">
-                    <i class="fab fa-github"></i>
+                    <i class="fab fa-github fa-2x"></i>
                 </a>
             </div>
             <p class="text-center text-white m-3">© 2025 Company, Inc</p>
