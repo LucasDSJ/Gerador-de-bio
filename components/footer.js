2db4b34361c4f4f321f9671ae317651d81780326
LucasDSJ <lucasdsjr2005@gmail.com>
2025-10-18 02:09:35 -0300

    Acrescentado modificações em contato.html e style.css

--- components/footer.js
+++ components/footer.js
@@ -2,11 +2,11 @@ const FooterComponent = {
     template: `
         <footer class="border-top text-center text-white py-3 mt-5" style="background: rgb(71, 53, 142);">
             <div class="border-bottom p-3">
-                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button">
-                    <i class="fab fa-linkedin-in fa-2x"></i>
+                <a class="btn btn-outline-light btn-floating m-1" href="https://www.linkedin.com/in/lucas-da-silva-junior-58b6a728b/" target="_blank" role="button">
+                    <i class="fab fa-linkedin-in fa-2x pt-1"></i>
                 </a>
                 <a class="btn btn-outline-light btn-floating m-1" href="https://github.com/LucasDSJ" target="_blank" role="button">
-                    <i class="fab fa-github fa-2x"></i>
+                    <i class="fab fa-github fa-2x pt-1"></i>
                 </a>
             </div>
             <p class="text-center text-white m-3">© 2025 Company, Inc</p>
