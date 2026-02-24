36d21b824431e8fc50a634a59a8c98e3fdc34725
LucasDSJ <lucasdsjr2005@gmail.com>
2026-02-24 02:22:53 -0300

    Teste de correção de caminho.
    Isso foi feito para corrigir um bug que levava o usuário ao erro 404 ao clicar na logo do site (deveria voltar para a página principal).

--- components/navbar.js
+++ components/navbar.js
@@ -2,7 +2,7 @@ const NavbarComponent = {
     template: `
         <nav class="navbar navbar-expand-lg border-bottom border-light p-3" data-bs-theme="dark" style="background: rgb(71, 53, 148);">
         <div class="container-fluid">
-            <a class="navbar-brand" style="font-size: 25pt" href="/index.html" >Gerador de Bio</a>
+            <a class="navbar-brand" style="font-size: 25pt" href="./index.html" >Gerador de Bio</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
